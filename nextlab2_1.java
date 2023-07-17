99516449724264716c6ef0ca91bf46f74849fa71
Barry-kamo <122296148+Barry-kamo@users.noreply.github.com>
2023-07-17 13:27:38 +0700

    Add files via upload

---
+++ nextlab2_1.java
@@ -0,0 +1,10 @@
+//请阅读注释然后删除备注
+//修改file名和class名字nextlab2_1为对应的作业名字
+public class nextlab2_1{
+    public static void main(String[] args) {
+        System.out.println("My name is ");//添加名字
+        System.out.println("I am a student in Thailand");
+        System.out.println("My hometown is ");//添加家乡
+        System.out.println("My favorite artist is ");//添加艺术家
+    }
+}
---
+++ nextlab2_2.java
@@ -0,0 +1,9 @@
+//请阅读注释然后删除备注
+//修改file名和class名字nextlab2_1为对应的作业名字
+public class nextlab2_1{
+    public static void main(String[] args) {
+        System.out.println(5*4);
+        System.out.println(8*10.5/2);
+        System.out.println(Math.pow(12.5, 2)*Math.PI);
+    }
+}
