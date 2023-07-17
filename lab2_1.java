d97fd73a6cc8580b7e0e27ee510754f74b0c7f68
Barry-kamo <122296148+Barry-kamo@users.noreply.github.com>
2023-07-17 13:10:00 +0700

    Add files via upload

---
+++ lab2_1.java
@@ -0,0 +1,11 @@
+public class lab2_1 {
+    public static void main(String[] args) {
+        System.out.print("Three...");
+        System.out.print("two...");
+        System.out.print("one...");
+        System.out.print("zero...");
+
+        System.out.println("2010");
+         System.out.println("This is My First JAVA program:");
+    }
+}
